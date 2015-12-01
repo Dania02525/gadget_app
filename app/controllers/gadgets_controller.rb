8495bd38389db77e261845b0d9c9c4e76b147f63
dania02525 <dania02525@gmail.com>
2015-12-01 16:35:30 -0600

    override gadget url param for fun

--- app/controllers/gadgets_controller.rb
+++ app/controllers/gadgets_controller.rb
@@ -65,7 +65,8 @@ class GadgetsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_gadget
-      @gadget = Gadget.find(params[:id])
+      id = params[:id].split("-").last
+      @gadget = Gadget.find(id)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/gadget.rb
+++ app/models/gadget.rb
@@ -4,6 +4,10 @@ class Gadget < ActiveRecord::Base
   
   after_create :start_gadget_factory
 
+  def to_param
+    "gadget-#{id}"
+  end
+
 
   def start_gadget_factory
     GadgetFactory.perform_async(self.id)
